6ab9d0821375000457cdb175a7143f70399aaeda
Andhiki <77545971+Andhiki@users.noreply.github.com>
2024-07-21 20:01:49 +0700

    fix footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -4,27 +4,29 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <footer className="bg-basicLightGreen-10 p-5 border-red-600 z-20 text-white ">
-            <div className="bg-basicBlack-10 rounded-lg p-10 pt-20 flex flex-col items-center gap-4">
-                {/* Logo OemOem */}
-                <Image
-                    src="footerLogo.svg"
-                    alt="menu"
-                    width={164}
-                    height={32}
-                    className="inline-block cursor-pointer"
-                />
+            <div className="bg-basicBlack-10 rounded-lg p-10 pt-20 flex flex-col items-center gap-4 lg:flex-row lg:justify-between lg:items-center lg:pt-10">
+                <div className="flex flex-col items-center gap-10">
+                    {/* Logo OemOem */}
+                    <Image
+                        src="footerLogo.svg"
+                        alt="menu"
+                        width={164}
+                        height={32}
+                        className="inline-block cursor-pointer"
+                    />
 
-                {/* Presented By */}
-                <Image
+                    {/* Presented By */}
+                    <Image
                         src="presented.svg"
                         alt="menu"
                         width={320}
                         height={32}
                         className="inline-block cursor-pointer"
                     />
+                </div>
 
                 {/* Socials */}
-                <div className="mt-10 flex flex-col gap-5 items-center">
+                <div className="mt-10 flex flex-col gap-5 items-center lg:items-stretch lg:mt-0 lg:mr-10 md:flex-row lg:flex-col">
                     <div className="flex items-center gap-2">
                         <Image
                             src="instagram.svg"
