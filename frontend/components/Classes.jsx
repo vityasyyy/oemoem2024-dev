d2b6b613c98a95cff9309ebb32fa8b903f6e2429
vityasyyy <argyavityasy1208@gmail.com>
2024-07-23 01:10:18 +0700

    fixed import name

--- frontend/components/Classes.jsx
+++ frontend/components/Classes.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useRouter } from 'next/navigation';
 
-const Kelas = ({ events }) => {
+const Classes = ({ events }) => {
     const router = useRouter();
 
     const handleClick = (id) => {
