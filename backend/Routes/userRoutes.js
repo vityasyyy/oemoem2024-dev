d97b09e3130e95fa9155c53affa596f71eebceeb
vityasyyy <argyavityasy1208@gmail.com>
2024-07-22 18:39:30 +0700

    Added user validation

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router({mergeParams: true})
-const {login, register, logout} = require('../Controllers/userController');
+const {login, register, logout, validate} = require('../Controllers/userController');
 
 router.post('/register', register);
 router.post('/login', passport.authenticate('local'),login);
 router.post('/logout', logout);
+router.post('/validate', validate);
 
 module.exports = router;
