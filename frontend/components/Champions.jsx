79b416ceea4d4df039bd2bb9efa403e584ca5c40
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2024-07-25 22:41:58 +0700

    /kelas

---
+++ frontend/components/Champions.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Champions = () => {
+    return (
+        <>
+            <p className="text-white">Champions dipersembahkan bagi top-3 pemenang challenge untuk masing-masing kelas</p>
+        </>
+    )
+};
+
+export default Champions;
